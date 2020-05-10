5679aaf4663c497e8e8d6a745ba2304ed3a068e0
peter <peter_jiachen@163.com>
2020-05-10 15:51:32 +0800

    Separate scripts

---
+++ win_conda_conda_test.bat
@@ -0,0 +1,13 @@
+@echo on
+
+call install_conda.bat
+if errorlevel 1 exit /b 1
+
+call install_pytorch_by_conda.bat
+if errorlevel 1 exit /b 1
+
+call patch_init_script.bat
+if errorlevel 1 exit /b 1
+
+call test_pytorch_repos_import.bat
+if errorlevel 1 exit /b 1
---
+++ win_python_pip_test.bat
@@ -0,0 +1,13 @@
+@echo on
+
+call install_python.bat
+if errorlevel 1 exit /b 1
+
+call install_pytorch_by_wheels.bat
+if errorlevel 1 exit /b 1
+
+call patch_init_script.bat
+if errorlevel 1 exit /b 1
+
+call test_pytorch_repos_import.bat
+if errorlevel 1 exit /b 1
