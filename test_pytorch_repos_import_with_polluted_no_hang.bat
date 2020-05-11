fbb3088964a0599b72e4cbfba81bb4884c85ab5e
peter <peter_jiachen@163.com>
2020-05-11 17:38:03 +0800

    More attempts

--- test_pytorch_repos_import_with_polluted_no_hang.bat
+++ test_pytorch_repos_import_with_polluted_no_hang.bat
@@ -5,7 +5,7 @@ if not exist polluted_dlls.7z (
     if errorlevel 1 exit /b 1
 )
 
-for /f "usebackq tokens=*" %%i in (`python.exe -c "import os, importlib; print(os.path.dirname(importlib.util.find_spec('torch').origin))"`) do (
+for /f "usebackq tokens=*" %%i in (`python.exe -c "import os, importlib; import importlib.util; print(os.path.dirname(importlib.util.find_spec('torch').origin))"`) do (
     set "PYTORCH_ROOT=%%i"
 )
 
