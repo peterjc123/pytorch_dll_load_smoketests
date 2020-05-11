08d5a297245ac71274baaf16349b72d2b9c0d0b1
peter <peter_jiachen@163.com>
2020-05-11 17:31:31 +0800

    Alternative

--- patch_init_script.bat
+++ patch_init_script.bat
@@ -3,7 +3,7 @@
 where python.exe
 if errorlevel 1 exit /b 1
 
-for /f "usebackq tokens=*" %%i in (`python.exe -c "import os, importlib; print(os.path.dirname(importlib.machinery.PathFinder().find_spec('torch').origin))"`) do (
+for /f "usebackq tokens=*" %%i in (`python.exe -c "import os, importlib; print(os.path.dirname(importlib.util.find_spec('torch').origin))"`) do (
     set "PYTORCH_ROOT=%%i"
 )
 
