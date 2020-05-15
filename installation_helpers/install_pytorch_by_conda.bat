0749ebaebae61d445a996022d435c2b7126d0307
peter <peter_jiachen@163.com>
2020-05-15 14:59:06 +0800

    Restructure project

--- installation_helpers/install_pytorch_by_conda.bat
+++ installation_helpers/install_pytorch_by_conda.bat
@@ -6,6 +6,6 @@ if "%CUDA_VERSION%" == "cpu" (
     set CONDA_FEATURE_TRACKING=cudatoolkit=%CUDA_VERSION%
 )
 
-conda install -y %CONDA_FEATURE_TRACKING% pytorch torchvision -c pytorch-nightly
+call conda install -y %CONDA_FEATURE_TRACKING% pytorch torchvision -c pytorch-nightly
 if errorlevel 1 exit /b 1
 
